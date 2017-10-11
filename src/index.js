db0f255ff9abac3aa96c82a05361723fda3e7146
David Waring <dev@davidwaring.net>
2017-10-11 19:35:32 -0400

    Update module export definitions

--- src/index.js
+++ src/index.js
@@ -1,6 +1,18 @@
 'use strict';
 
+/**
+ * ### Right Track Agency (Abstract)
+ * This module contains classes and functions that are shared across
+ * all implementations of a Right Track Agency
+ * @module /
+ */
 
 module.exports = {
+
+  /**
+   * Shared agency configuration functions
+   * @see module:config
+   */
   config: require('./config.js')
+
 };
